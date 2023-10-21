30f2a1a52e5f67090e333193217ee0e3c343f837
Sessiom <caufaglj2@newpaltz.edu>
2023-10-21 17:47:46 -0400

    Created NewWorkoutPost component

---
+++ client/src/components/NewWorkoutPost.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { ref } from "vue"
+const show1 = ref(true);
+</script>
+
+
+<template>
+
+<div v-if="show1">
+    <article class="media box">
+      <figure class="media-left">
+        <p class="image is-64x64"><img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMo3I5GL9_Zd_LULXRIXTzRLlVESBnoGp8sw&amp;usqp=CAU">
+        </p>
+      </figure>
+      <div class="media-content">
+      <div class="content">
+        <p><strong>Moshe Plotkin</strong> &nbsp;<small>@JewPaltz</small> &nbsp; <small>24 min ago</small><br>
+          Biked through campus - {
+          "lat": 41.7459793,
+          "lng": -74.082801
+          }
+        <div class="columns">
+          <div class="column has-text-centered"
+            style="display: flex; justify-content: space-around; align-items: center;">
+            <div>
+              <div class="title" style="margin: 0px;">1.0 mi</div>
+              <div class="heading">Distance</div>
+            </div>
+            <div>
+              <div class="title" style="margin: 0px;">0:30</div>
+              <div class="heading">Duration</div>
+            </div>
+          </div>
+          <div class="column"><img
+              src="https://www.rei.com/dam/content_team_080317_61569_mountain_biking_beginners_lg.jpg"
+              style="max-height: 100%;"></div>
+        </div>
+        </p>
+      </div>
+      <nav class="level is-mobile">
+        <div class="level-left"><a class="level-item"><span class="icon is-small"><i
+                class="fas fa-reply"></i></span></a><a class="level-item"><span class="icon is-small"><i
+                class="fas fa-retweet"></i></span></a><a class="level-item"><span class="icon is-small"><i
+                class="fas fa-heart"></i></span></a></div>
+      </nav>
+    </div>
+    <div class="media-right"><button @click="show1 = !show1" class="delete"></button></div>
+  </article>
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
+
--- client/src/views/MyActivity.vue
+++ client/src/views/MyActivity.vue
@@ -1,59 +1,31 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-
-  const newTask = ref('');
-  const tasks = ref([] as { id?: number, text: string, completed: boolean }[] );
-
-  const tabList = ['Current', 'Completed', 'All'];
-  const tabState = ref('Current');
-
-  function addTask() {
-    tasks.value.push({ text: newTask.value, completed: false });
-    newTask.value = '';
-  };
-
-  const shouldDisplay = (task: { id?: number, text: string, completed: boolean }) =>
-    (tabState.value == 'Current' && !task.completed) ||
-    (tabState.value == 'Completed' && task.completed) ||
-    tabState.value == 'All';
-
-
+import { ref } from "vue"
+import AddWorkoutForm from "@/components/AddWorkoutForm.vue"
+import NewWorkoutPost from "@/components/NewWorkoutPost.vue";
+const isActive = ref(false);
 </script>
 
 <template>
-  <main class="columns is-multiline is-centered">
-    <div class="column is-full">
-      <h1 class="title" >My Activity</h1>
-      
-    </div>
 
-    <div class="column is-half-desktop is-centered">
-      <div class="panel is-primary">
-        <p class="panel-heading">
-          To Do
-        </p>
-        <div class="panel-block">
-          <p class="control has-icons-left">
-            <input  class="input" type="text" placeholder="What do you want to do"
-                    @keypress.enter="addTask" v-model="newTask" >
-            <span class="icon is-left">
-              <i class="fas fa-plus" aria-hidden="true"></i>
-            </span>
-          </p>
-        </div>
-        <p class="panel-tabs">
-          <a v-for="tab in tabList" :class="{ 'is-active': tabState == tab}" @click.prevent="tabState = tab">{{ tab }}</a>
-        </p>
-        <label class="panel-block" v-for="task in tasks" v-show="shouldDisplay(task)">
-          <input type="checkbox" v-model="task.completed">
-          {{ task.text }}
-        </label>
-        <div class="panel-block">
-          <button class="button is-link is-outlined is-fullwidth">
-            Reset all filters
-          </button>
-        </div>
+  
+    <div>
+      <h1 class="title">Friends Activity</h1>
+      <div class="columns">
+        <div class="column is-half is-offset-one-quarter">
+          <button @click="isActive = !isActive" class="button is-info is-fullwidth">Add Workout</button>
+          <form v-if="isActive">
+           <AddWorkoutForm/>
+          </form><br>   
+        </div> 
       </div>
+      <NewWorkoutPost/>
     </div>
-  </main>
 </template>
+
+
+
+<style scoped>
+
+</style>
+
+
