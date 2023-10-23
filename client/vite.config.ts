0dc4309319891f9211756d3ab7b08c25bb2b3d4a
Sessiom <caufaglj2@newpaltz.edu>
2023-10-22 22:43:25 -0400

    fixed bug with importing data

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,13 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        input: '/public/index.html',
+      }
+    }
+  },
   plugins: [
     vue(),
     vueJsx(),
