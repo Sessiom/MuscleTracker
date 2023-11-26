02ee0385895ad6851c99b5e61f5baae2be8bb794
Sessiom <caufaglj2@newpaltz.edu>
2023-11-26 11:35:31 -0500

    implemented Post method in Fetch

--- client/src/model/myFetch.ts
+++ client/src/model/myFetch.ts
@@ -1,16 +1,25 @@
 /* B"H
 */
 
-const API_ROOT = 'http://localhost:3000/api/v1';
+const API_ROOT = import.meta.env.VITE_API_ROOT as string;
 
-export function rest(url: string){
-    return fetch(url)
-        .then(response => response.json())
-        .catch(err => console.log(err));
+export function rest(url: string, body?: unknown, method?: string){
+    return fetch(url, {
+        method: method ?? (body ? "POST" : "GET"),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: body ? JSON.stringify(body) : undefined
+    })
+        .then(response => response.ok 
+            ? response.json()
+            : response.json().then(err => Promise.reject(err))
+        )
+
 }
 
-export function api(action: string){
-    return rest(`${API_ROOT}/${action}`);
+export function api(action: string, body?: unknown, method?: string){
+    return rest(`${API_ROOT}/${action}`, body, method);
 }
 
 
