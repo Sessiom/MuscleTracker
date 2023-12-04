9fb97e4073c522fc7fb6c0a8e1c20ff89d6a2279
Sessiom <caufaglj2@newpaltz.edu>
2023-12-03 20:48:37 -0500

    Added Workouts.json & made interface

--- client/src/model/workout.ts
+++ client/src/model/workout.ts
@@ -1,6 +1,6 @@
 import { ref, onMounted } from "vue";
 import { defineProps } from "vue";
-
+import data from "../data/workouts.json"
 onMounted(() => {
     const savedWorkouts = JSON.parse(localStorage.getItem("workouts") || "[]");
 
@@ -21,46 +21,16 @@ const props = defineProps({
     }
 });
 
-export const workouts = ref([
-    {id: 1, 
-     firstName: "Terry",
-     lastName: "Medhurst",
-     userName: "atuny0",
-     image: "https://robohash.org/hicveldicta.png",
-     title: "Parkour Training",
-     date: "5 min ago",
-     distance: "1.12 mi",
-     duration: "25:30",
-     location: "San Francisco, CA",
-     picture: "https://images.ctfassets.net/uwf0n1j71a7j/5GhVEraYQrWD6pA5JPIoYF/30e9b4a2ef2d10f43753aab65f31f7ff/parkour.jpg",
+export interface workout {
+    id: number,
+    firstName: string,
+    lastName: string,
+    userName: string,
+    image: string,
+    distance: string,
+    duration: string,
+    location: number,
+    picture: string
+  }
 
-    },
-    {id: 2, 
-     firstName: "Sheldon",
-     lastName: "Quigley",
-     userName: "hbingley1",
-     image: "https://robohash.org/doloremquesintcorrupti.png",
-     title: "Downhill Biking",
-     date: "1 hour ago",
-     distance: "1.24 mi",
-     duration: "6:21",
-     location: "San Francisco, CA",
-     picture: "https://www.twowheeledwanderer.com/wp-content/uploads/2021/07/Downhill-mountain-biking-2.jpg",
-     
-    },
-    {id: 3, 
-     firstName: "Terrill",
-     lastName: "Hills",
-     userName: "rshawe2",
-     image: "https://robohash.org/consequunturautconsequatur.png",
-     title: "Breakdancing",
-     date: "2 days ago",
-     distance: "0.23 mi",
-     duration: "11:58",
-     location: "San Francisco, CA",
-     picture: "https://img.redbull.com/images/c_limit,w_1500,h_1000,f_auto,q_auto/redbullcom/2019/10/11/8e9ca35b-f25c-426a-bb40-338feabff9cb/world-urban-games-2019-shigekix-chairfreeze",
-     
-    }
-]);
-
-
+export const workouts = ref(data.workouts);
